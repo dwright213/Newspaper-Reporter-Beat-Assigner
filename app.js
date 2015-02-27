b7cebc22fcd90f383a1dfd3a108884b60e403657
Dan Wright <muscle.crab.13@gmail.com>
2015-02-27 10:14:11 -0800

    add beat listing and controller

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var newsBeats = angular.module('newsBeats', ['ui.router']);
 newsBeats.config(function($stateProvider) {
   $stateProvider.state('home', {
     url: "",
-    templateUrl: "partials/home.html"
+    templateUrl: "partials/beats.html",
+    controller: 'BeatsCtrl'
   });
 });
